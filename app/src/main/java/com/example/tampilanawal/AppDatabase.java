0c8379674c12bbba27a02b9c9907804dea507fb2
iqbaalt07 <pudge.warcraft3@gmail.com>
2020-05-01 23:18:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tampilanawal/AppDatabase.java
@@ -0,0 +1,10 @@
+package com.example.tampilanawal;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {AktivisEntity.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract com.example.tampilanawal.AktivisDao aktivisDao();
+}
